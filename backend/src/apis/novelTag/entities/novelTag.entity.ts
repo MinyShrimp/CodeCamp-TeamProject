cb491f297c1a0446d9c1cda6cb88848737089845
MinyShrimp <ksk7584@gmail.com>
2022-07-04 15:58:42 +0900

    :recycle: DB Table 이름 변경

--- backend/src/apis/novelTag/entities/novelTag.entity.ts
+++ backend/src/apis/novelTag/entities/novelTag.entity.ts
@@ -9,7 +9,7 @@ import {
 } from 'typeorm';
 
 /* NovelTag Entity */
-@Entity({ name: 'novelTag' })
+@Entity({ name: 'novel_tag' })
 @ObjectType({ description: 'NovelTag Entity' })
 export class NovelTagEntity {
     @PrimaryGeneratedColumn('uuid')
