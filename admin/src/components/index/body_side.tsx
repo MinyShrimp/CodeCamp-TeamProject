9245ef1a605b687c942373df38e6ba51ae3c04c5
MinyShrimp <ksk7584@gmail.com>
2022-07-04 18:56:32 +0900

    :recycle: 사이드메뉴 수정 및 추가

--- admin/src/components/index/body_side.tsx
+++ admin/src/components/index/body_side.tsx
@@ -105,72 +105,235 @@ export function IndexBodySide() {
                                 <>
                                     {/****************************************************************************/}
                                     {/* USER */}
-                                    <Nav.Link as={Link} to="/admin/entity/user">
-                                        회원
-                                    </Nav.Link>
-                                    <Nav.Link
-                                        as={Link}
-                                        to="/admin/entity/userClass"
-                                    >
-                                        회원 등급
-                                    </Nav.Link>
-                                    <Nav.Link
-                                        as={Link}
-                                        to="/admin/entity/authPhone"
-                                    >
-                                        핸드폰 인증
-                                    </Nav.Link>
-                                    <Nav.Link
-                                        as={Link}
-                                        to="/admin/entity/authEmail"
-                                    >
-                                        이메일 인증
-                                    </Nav.Link>
-                                    <hr />
+                                    <NavCollapse
+                                        id="entity-user-collapes"
+                                        title="회원"
+                                        items={
+                                            <>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/user"
+                                                >
+                                                    회원
+                                                </Nav.Link>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/userClass"
+                                                >
+                                                    회원 등급
+                                                </Nav.Link>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/authPhone"
+                                                >
+                                                    핸드폰 인증
+                                                </Nav.Link>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/authEmail"
+                                                >
+                                                    이메일 인증
+                                                </Nav.Link>
+                                            </>
+                                        }
+                                    />
 
                                     {/****************************************************************************/}
                                     {/* PRODUCT */}
-                                    <Nav.Link
-                                        as={Link}
-                                        to="/admin/entity/product"
-                                    >
-                                        상품
-                                    </Nav.Link>
-                                    <Nav.Link
-                                        as={Link}
-                                        to="/admin/entity/payment"
-                                    >
-                                        결제
-                                    </Nav.Link>
-                                    <Nav.Link
-                                        as={Link}
-                                        to="/admin/entity/paymentStatus"
-                                    >
-                                        결제 상태
-                                    </Nav.Link>
-                                    <hr />
+                                    <NavCollapse
+                                        id="entity-product-collapes"
+                                        title="상품"
+                                        items={
+                                            <>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/product"
+                                                >
+                                                    상품
+                                                </Nav.Link>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/payment"
+                                                >
+                                                    결제
+                                                </Nav.Link>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/paymentStatus"
+                                                >
+                                                    결제 상태
+                                                </Nav.Link>
+                                            </>
+                                        }
+                                    />
 
                                     {/****************************************************************************/}
                                     {/* QUESTION */}
-                                    <Nav.Link
-                                        as={Link}
-                                        to="/admin/entity/question"
-                                    >
-                                        문의
-                                    </Nav.Link>
-                                    <Nav.Link
-                                        as={Link}
-                                        to="/admin/entity/answer"
-                                    >
-                                        답변
-                                    </Nav.Link>
-                                    <hr />
+                                    <NavCollapse
+                                        id="entity-question-collapes"
+                                        title="문의"
+                                        items={
+                                            <>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/question"
+                                                >
+                                                    문의
+                                                </Nav.Link>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/answer"
+                                                >
+                                                    답변
+                                                </Nav.Link>
+                                            </>
+                                        }
+                                    />
+
+                                    {/****************************************************************************/}
+                                    {/* BOARD */}
+                                    <NavCollapse
+                                        id="entity-board-collapes"
+                                        title="게시판"
+                                        items={
+                                            <>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/notice"
+                                                >
+                                                    공지
+                                                </Nav.Link>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/event"
+                                                >
+                                                    이벤트
+                                                </Nav.Link>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/board"
+                                                >
+                                                    자유 게시판
+                                                </Nav.Link>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/comment"
+                                                >
+                                                    자유 게시판 댓글
+                                                </Nav.Link>
+                                            </>
+                                        }
+                                    />
+
+                                    {/****************************************************************************/}
+                                    {/* NOVEL */}
+                                    <NavCollapse
+                                        id="entity-novel-collapes"
+                                        title="소설"
+                                        items={
+                                            <>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/novel"
+                                                >
+                                                    소설
+                                                </Nav.Link>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/novelReview"
+                                                >
+                                                    소설 리뷰
+                                                </Nav.Link>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/novelIndex"
+                                                >
+                                                    소설 인덱스
+                                                </Nav.Link>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/novelIndexReview"
+                                                >
+                                                    편당 리뷰
+                                                </Nav.Link>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/novelTag"
+                                                >
+                                                    소설 태그
+                                                </Nav.Link>
+                                            </>
+                                        }
+                                    />
+
+                                    {/****************************************************************************/}
+                                    {/* POINT PAYMENT */}
+                                    <NavCollapse
+                                        id="entity-point-payment-collapes"
+                                        title="포인트 결제"
+                                        items={
+                                            <>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/pointPayment"
+                                                >
+                                                    포인트 결제
+                                                </Nav.Link>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/pointPaymentStatus"
+                                                >
+                                                    포인트 결제 상태
+                                                </Nav.Link>
+                                            </>
+                                        }
+                                    />
+
+                                    {/****************************************************************************/}
+                                    {/* 소설과 유저 연결 */}
+                                    <NavCollapse
+                                        id="entity-novel-user-collapes"
+                                        title="소설과 유저"
+                                        items={
+                                            <>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/novelDonate"
+                                                >
+                                                    후원작
+                                                </Nav.Link>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/novelLike"
+                                                >
+                                                    선호작
+                                                </Nav.Link>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/bookMark"
+                                                >
+                                                    북마크
+                                                </Nav.Link>
+                                            </>
+                                        }
+                                    />
 
                                     {/****************************************************************************/}
                                     {/* FILE */}
-                                    <Nav.Link as={Link} to="/admin/entity/file">
-                                        파일
-                                    </Nav.Link>
+                                    <NavCollapse
+                                        id="entity-file-collapes"
+                                        title="파일"
+                                        items={
+                                            <>
+                                                <Nav.Link
+                                                    as={Link}
+                                                    to="/admin/entity/file"
+                                                >
+                                                    파일
+                                                </Nav.Link>
+                                            </>
+                                        }
+                                    />
                                 </>
                             }
                         ></NavCollapse>
