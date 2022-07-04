18eb0272cae391b2adfaf69c26ed8579f9e77118
MinyShrimp <ksk7584@gmail.com>
2022-07-04 18:02:05 +0900

    :truck: Shell Script 수정"

--- admin/createEntity.sh
+++ admin/createEntity.sh
@@ -10,67 +10,56 @@ mkdir $APIDIR
 ##############################################################################
 # interface
 INTERFACE_FILE=$APIDIR/interface.tsx
-echo "
-const now = new Date();
+echo "import { getDefaultDate } from '../../../functions/functions';
+import { Dummy${UPPER}ClassColumn } from '../${FILENAME}/interface';
+
+const now = getDefaultDate();
+
 // prettier-ignore
 export const Dummy${UPPER}Column = {
-    id: '',
+    id: '', 
 };
 export type I${UPPER}Column = typeof Dummy${UPPER}Column;
 " >> $INTERFACE_FILE
 
 ##############################################################################
-# columns
-COLUMNS_FILE=$APIDIR/columns.tsx
-echo "
-import { createColumns } from '../../../functions/createColumns';
-import { Dummy${UPPER}Column } from './interface';
-
-// prettier-ignore
-export const ${UPPER}ColumnConfig = {
-    listColumns: [
-        'id', 
-    ],
-    showColumns: [
-        'id',
-    ],
-};
-
-export const List${UPPER}Columns = createColumns(
-    Dummy${UPPER}Column,
-    ${UPPER}ColumnConfig.listColumns,
-);
-
-export const Show${UPPER}Columns = createColumns(
-    Dummy${UPPER}Column,
-    ${UPPER}ColumnConfig.showColumns,
-);
-" >> $COLUMNS_FILE
-
-##############################################################################
 # index
 INDEX_FILE=$APIDIR/index.tsx
 echo "
-import React, { useEffect } from 'react';
-import { EntityIndex } from '../entity_index';
-import { List${UPPER}Columns, Show${UPPER}Columns } from './columns';
+import { EntityFactory } from '../entity_factory';
+import { I${UPPER}Column, Dummy${UPPER}Column } from './interface';
 
-export function ${UPPER}Index(props: {
-    setReload: Function;
-    setEntityName: Function;
-}) {
-    useEffect(() => {
-        props.setEntityName('${UPPER}');
-    }, []);
-
-    return (
-        <EntityIndex
-            setReload={props.setReload}
-            ListUrl=\"/admin/${FILENAME}s\"
-            ListColumns={List${UPPER}Columns}
-            ShowUrl=\"/admin/${FILENAME}\"
-            ShowColumns={Show${UPPER}Columns}
-        />
-    );
-}
+// prettier-ignore
+export const ${UPPER}Index = EntityFactory.getEntity<I${UPPER}Column>({
+    name: '',
+    dummyData: Dummy${UPPER}Column,
+    beURL: '/admin/${FILENAME}',
+    baseURL: '/admin/entity/${FILENAME}',
+    list: {
+        column: [
+            'id', 
+        ],
+    },
+    show: {
+        column: [
+            'id', 
+        ],
+    },
+    edit: {
+        column: [
+            'id', 
+        ],
+        default: {
+            id: '', 
+        }
+    },
+    update: {
+        column: [
+            'id', 
+        ],
+        default: {
+            id: '', 
+        }
+    }
+});
 " >> $INDEX_FILE
