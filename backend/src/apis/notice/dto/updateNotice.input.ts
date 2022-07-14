8df9beb938ceeae349db9fbd227866b3550abee3
KIMSEONGRAN <doll950904@gmail.com>
2022-07-14 10:49:50 +0900

    :sparkles: Board_CRUD_API 추가

---
+++ backend/src/apis/notice/dto/updateNotice.input.ts
@@ -0,0 +1,10 @@
+import { Field, InputType, PartialType } from '@nestjs/graphql';
+import { CreateNoticeInput } from './createNotice.input';
+
+@InputType()
+export class UpdateNoticeInput extends PartialType(
+    CreateNoticeInput, //
+) {
+    @Field(() => String, { description: '원본 공지사항 UUID' })
+    id: string;
+}
---
+++ backend/src/apis/notice/notice.service.ts
@@ -0,0 +1,136 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+
+import { MESSAGES } from 'src/commons/message/Message.enum';
+import { UserRepository } from '../user/entities/user.repository';
+
+import { NoticeEntity } from './entities/notice.entity';
+import { NoticeRepository } from './entities/notice.repository';
+import { CreateNoticeInput } from './dto/createNotice.input';
+import { UpdateNoticeInput } from './dto/updateNotice.input';
+
+@Injectable()
+export class NoticeService {
+    constructor(
+        private readonly noticeRepository: NoticeRepository, //
+        private readonly userRepository: UserRepository,
+    ) {}
+
+    /** 관리자 여부 판별 */
+    async checkAdmin(
+        userID: string, //
+    ) {
+        const user = await this.userRepository.findOneByID(userID);
+
+        if (user.userClass.id !== 'ADMIN' && user.userClass.id !== 'SUB_ADMIN')
+            throw new ConflictException(
+                MESSAGES.NOTICE_AUTHORIZATION, //
+            );
+
+        return user;
+    }
+
+    /** 삭제 확인 */
+    async checkValidWithDeleted(
+        userID: string, //
+        noticeID: string,
+    ): Promise<NoticeEntity> {
+        const notice = await this.noticeRepository.getIDWithUserWithDeleted(
+            userID,
+            noticeID,
+        );
+        const deleteData = await this.noticeRepository.find();
+
+        // prettier-ignore
+        const nullData = deleteData.map((data) => { return data });
+
+        // prettier-ignore
+        if (
+            notice === undefined ||
+            !nullData.map((test) => { return test.id }).includes(noticeID) &&
+            nullData.map((test) => { return test.deleteAt }) === null
+        ) {
+            throw new ConflictException(MESSAGES.NOTICE_UNVALID);
+        }
+        return notice;
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    // 조회 //
+
+    /** 모든 공지 조회 */
+    async findAll(): Promise<NoticeEntity[]> {
+        return await this.noticeRepository.findAll();
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    // 생성
+
+    async createNotice(
+        userID: string, //
+        input: CreateNoticeInput,
+    ): Promise<NoticeEntity> {
+        // 관리자 여부 판별
+        const user = await this.checkAdmin(userID);
+
+        return await this.noticeRepository.save({
+            user,
+            ...input,
+        });
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    // 수정
+
+    async updateNotice(
+        userID: string,
+        input: UpdateNoticeInput, //
+    ): Promise<NoticeEntity> {
+        const { id, ...rest } = input;
+        const notice = await this.noticeRepository.findOneByID(id);
+
+        // 공지사항 존재 여부 확인
+        if (!notice) throw new ConflictException(MESSAGES.NOTICE_UNVALID);
+
+        // 관리자 여부 판별
+        await this.checkAdmin(userID);
+
+        return await this.noticeRepository.save({
+            ...notice,
+            ...rest,
+        });
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    // 삭제 //
+
+    async softDelete(
+        userID: string,
+        noticeID: string, //
+    ): Promise<string> {
+        // 관리자 여부 판별
+        await this.checkAdmin(userID);
+
+        const result = await this.noticeRepository.softDelete(noticeID);
+
+        return result.affected
+            ? MESSAGES.NOTICE_SOFT_DELETE_SUCCESSED
+            : MESSAGES.NOTICE_SOFT_DELETE_FAILED;
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    // 삭제 취소 //
+
+    async restore(
+        userID: string, //
+        noticeID: string, //
+    ): Promise<boolean> {
+        // 관리자 여부 판별
+        await this.checkAdmin(userID);
+
+        // 삭제 여부 판별
+        await this.checkValidWithDeleted(userID, noticeID);
+
+        const result = await this.noticeRepository.restore(noticeID);
+        return result.affected ? true : false;
+    }
+}
