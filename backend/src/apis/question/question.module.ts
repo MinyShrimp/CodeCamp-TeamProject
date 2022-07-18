d0c02581adc67a62c85d5c7d4c547e4126469ac5
KIMSEONGRAN <doll950904@gmail.com>
2022-07-18 15:47:18 +0900

    :sparkles: 문의 CRUD API 추가

--- backend/src/apis/question/question.module.ts
+++ backend/src/apis/question/question.module.ts
@@ -2,21 +2,32 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { QuestionEntity } from './entities/question.entity';
-import { QuestionAdminRepository } from './entities/question.admin.repository';
-
+import { QuestionService } from './question.service';
+import { QuestionResolver } from './question.resolver';
+import { QuestionRepository } from './entities/question.repository';
 import { QuestionAdminController } from './question.admin.controller';
+import { QuestionAdminRepository } from './entities/question.admin.repository';
+import { UserRepository } from '../user/entities/user.repository';
+import { UserEntity } from '../user/entities/user.entity';
+import { UserModule } from '../user/user.module';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([
             QuestionEntity, //
+            UserEntity,
         ]),
+        UserModule,
     ],
     controllers: [
         QuestionAdminController, //
     ],
     providers: [
         QuestionAdminRepository, //
+        QuestionRepository,
+        QuestionResolver,
+        QuestionService,
+        UserRepository,
     ],
 })
 export class QuestionModule {}
