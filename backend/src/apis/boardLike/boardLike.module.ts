f73583955033817bf00993371219625ef755fac2
KIMSEONGRAN <doll950904@gmail.com>
2022-07-23 18:20:28 +0900

    :sparkles: 게시글 좋아요 테이블 및 API 추가

--- backend/src/apis/boardLike/boardLike.module.ts
+++ backend/src/apis/boardLike/boardLike.module.ts
@@ -1,22 +1,41 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { BoardLikeEntity } from './entities/boardLike.entity';
-import { BoardLikeAdminRepository } from './entities/boardLike.admin.repository';
+import { UserModule } from '../user/user.module';
+import { FileModule } from '../file/file.module';
+import { BoardModule } from '../board/board.module';
+import { BoardEntity } from '../board/entities/board.entity';
+import { BoardService } from '../board/board.service';
+import { BoardRepository } from '../board/entities/board.repository';
 
+import { BoardLikeEntity } from './entities/boardLike.entity';
+import { BoardLikeService } from './boardLike.service';
+import { BoardLikeResolver } from './boardLike.resolver';
+import { BoardLikeRepository } from './entities/boardLike.repository';
 import { BoardLikeAdminController } from './boardLike.admin.controller';
+import { BoardLikeAdminRepository } from './entities/boardLike.admin.repository';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([
             BoardLikeEntity, //
+            BoardEntity,
         ]),
+        BoardModule,
+        UserModule,
+        FileModule,
     ],
     controllers: [
         BoardLikeAdminController, //
     ],
     providers: [
         BoardLikeAdminRepository, //
+
+        BoardLikeRepository,
+        BoardLikeResolver,
+        BoardLikeService,
+        BoardRepository,
+        BoardService,
     ],
 })
 export class BoardLikeModule {}
--- backend/src/apis/boardLike/entities/boardLike.entity.ts
+++ backend/src/apis/boardLike/entities/boardLike.entity.ts
@@ -1,11 +1,43 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BoardEntity } from 'src/apis/board/entities/board.entity';
+import { UserEntity } from 'src/apis/user/entities/user.entity';
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
 
 /* BoardLike Entity */
-@Entity({ name: 'boardLike' })
+@Entity({ name: 'board_like' })
 @ObjectType({ description: 'BoardLike Entity' })
 export class BoardLikeEntity {
     @PrimaryGeneratedColumn('uuid')
-    @Field(() => ID)
+    @Field(() => ID, { description: 'UUID' })
     id: string;
+
+    @CreateDateColumn()
+    @Field(() => Date, { description: '생성 시간' })
+    createAt: Date;
+
+    @ManyToOne(
+        () => UserEntity, //
+        { cascade: true, onDelete: 'CASCADE' },
+    )
+    @Field(() => UserEntity)
+    user: UserEntity;
+
+    @Column({ name: 'userId', nullable: true })
+    userID: string;
+
+    @ManyToOne(
+        () => BoardEntity, //
+        { cascade: true, onDelete: 'CASCADE' },
+    )
+    @Field(() => BoardEntity)
+    board: BoardEntity;
+
+    @Column({ name: 'boardId', nullable: true })
+    boardID: string;
 }
