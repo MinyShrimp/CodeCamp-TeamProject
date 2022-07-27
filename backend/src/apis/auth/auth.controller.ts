465de9d51347c15141d911f7e2f0b72e78fae3d3
MinyShrimp <ksk7584@gmail.com>
2022-07-27 17:15:18 +0900

    :bug: OAuth Redirect 경로 수정

--- backend/src/apis/auth/auth.controller.ts
+++ backend/src/apis/auth/auth.controller.ts
@@ -16,7 +16,10 @@ interface IOAuthRequest extends Request {
 @ApiTags('인증')
 @Controller()
 export class AuthController {
-    private readonly REDIRECT = process.env.FE_URL;
+    private readonly REDIRECT =
+        process.env.MODE === 'PRODUCTION'
+            ? `${process.env.FE_URL}/socialLogin`
+            : process.env.FE_URL;
 
     constructor(
         private readonly userRepository: UserRepository,
